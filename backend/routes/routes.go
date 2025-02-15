42474a8901120661de1387315c4f5257beedb97a
Turgho <victor.hugo3692111@gmail.com>
2025-02-15 17:29:33 -0300

    update: new routes

--- backend/routes/routes.go
+++ backend/routes/routes.go
@@ -7,4 +7,11 @@ import (
 
 func SetupRoutes(r *gin.Engine) {
 	r.GET("/ping", handlers.PingHandler)
+
+	r.GET("/clientes", handlers.ListClientes)
+	r.POST("/clientes", handlers.RegistryCliente)
+	r.GET("/cliente", handlers.FindClienteById)
+
+	r.POST("/servicos", handlers.RegistryServico)
+	r.GET("/servicos", handlers.ListAllServicos)
 }
