f9c74a67bc422475d74a781391c94795945cf77e
Turgho <victor.hugo3692111@gmail.com>
2025-02-18 17:13:54 -0300

    update: route protection with JWT token

--- backend/routes/routes.go
+++ backend/routes/routes.go
@@ -2,16 +2,47 @@ package routes
 
 import (
 	"github.com/Turgho/barberfast/backend/handlers"
+	"github.com/Turgho/barberfast/backend/middleware"
 	"github.com/gin-gonic/gin"
 )
 
 func SetupRoutes(r *gin.Engine) {
-	r.GET("/ping", handlers.PingHandler)
+	v1 := r.Group("/v1")
+	{
+		// Rota pública (Login não requer autenticação)
+		v1.POST("/login", handlers.Login)
 
-	r.GET("/clientes", handlers.ListClientes)
-	r.POST("/clientes", handlers.RegistryCliente)
-	r.GET("/cliente", handlers.FindClienteById)
+		// Rotas que exigem autenticação (JWTAuthMiddleware)
+		v1.Use(middleware.JWTAuthMiddleware()) // Middleware aplicado a todas as rotas abaixo
 
-	r.POST("/servicos", handlers.RegistryServico)
-	r.GET("/servicos", handlers.ListAllServicos)
+		// Grupo admin com permissão extra
+		admin := v1.Group("/admin")
+		{
+			// Middleware adicional para verificar se é admin
+			admin.Use(middleware.AdminMiddleware())
+
+			// Rotas administrativas
+			admin.GET("/ping", handlers.PingHandler)
+
+			admin.GET("/clientes", handlers.ListAllUsuarios)
+			admin.GET("/cliente", handlers.FindUsuarioById)
+			admin.DELETE("/cliente", handlers.DeleteUsuario)
+
+			admin.POST("/servicos", handlers.RegistryServico)
+			admin.GET("/servicos", handlers.ListAllServicos)
+			admin.GET("/servico", handlers.FindServicoById)
+			admin.DELETE("/servico", handlers.DeleteServicoById)
+
+			admin.GET("/agendamento", handlers.FindAgendamentoById)
+			admin.GET("/agendamentos", handlers.ListAllAgendamentos)
+			admin.DELETE("/agendamento", handlers.DeleteAgendamentoById)
+		}
+
+		// Rotas para o usuário comum
+		usuario := v1.Group("/usuario")
+		{
+			usuario.POST("/clientes", handlers.RegistryUsuario)
+			usuario.POST("/agendamentos", handlers.RegistryAgendamento)
+		}
+	}
 }
