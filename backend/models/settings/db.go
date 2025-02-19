6cf5581c77835679452c5fa5eca40ffeeca3c03e
Turgho <victor.hugo3692111@gmail.com>
2025-02-19 13:29:24 -0300

    update: db documentation

--- backend/models/settings/db.go
+++ backend/models/settings/db.go
@@ -1,6 +1,7 @@
 package settings
 
 import (
+	"errors"
 	"fmt"
 	"log"
 	"os"
@@ -10,52 +11,64 @@ import (
 	"gorm.io/gorm"
 )
 
+// DBConnectionHandler é um tipo que encapsula a conexão com o banco de dados, permitindo a manipulação do objeto GORM.
 type DBConnectionHandler struct {
 	DB *gorm.DB
 }
 
+// DBConnect é responsável por estabelecer a conexão com o banco de dados, utilizando as credenciais do arquivo .env.
 func DBConnect() (*DBConnectionHandler, error) {
-	// Carregar o arquivo .env
+	// Carrega as variáveis de ambiente do arquivo .env
 	if err := godotenv.Load("../../.env"); err != nil {
-		log.Fatal("Erro ao carregar o arquivo .env: ", err)
+		log.Fatalf("Erro ao carregar o arquivo .env: %v", err)
 	}
 
-	// Pegando as credenciais do .env ou configurando manualmente
+	// Recupera as credenciais de conexão do banco de dados das variáveis de ambiente
 	dsn := fmt.Sprintf("%s:%s@tcp(%s)/%s?charset=utf8mb4&parseTime=True&loc=Local",
-		os.Getenv("DB_USER"),     // Usuário
-		os.Getenv("DB_PASSWORD"), // Senha
-		os.Getenv("DB_HOST"),     // Host
+		os.Getenv("DB_USER"),     // Usuário do banco de dados
+		os.Getenv("DB_PASSWORD"), // Senha do banco de dados
+		os.Getenv("DB_HOST"),     // Endereço do host do banco de dados
 		os.Getenv("DB_NAME"),     // Nome do banco de dados
 	)
-	// Abrindo conexão
+
+	// Tenta abrir a conexão com o banco de dados utilizando o driver MySQL
 	handler, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
-		return nil, fmt.Errorf("erro ao abrir conexão com banco de dados: %v", err)
+		log.Printf("Erro ao abrir conexão com o banco de dados: %v", err)
+		return nil, errors.New("erro ao abrir conexão com banco de dados")
 	}
 
-	// Conectando
+	// Obtém a instância do banco de dados subjacente
 	db, err := handler.DB()
 	if err != nil {
-		return nil, fmt.Errorf("erro ao conectar ao banco de dados: %v", err)
+		log.Printf("Erro ao acessar o banco de dados: %v", err)
+		return nil, errors.New("erro ao acessar banco de dados")
 	}
 
-	fmt.Println("Conectado ao Banco de Dados!")
+	log.Println("Conectado ao Banco de Dados!")
 
-	// Acessando
+	// Verifica se é possível acessar o banco de dados
 	if err := db.Ping(); err != nil {
-		return nil, fmt.Errorf("erro ao acessar banco de dados: %v", err)
+		log.Printf("Erro ao acessar o banco de dados: %v", err)
+		return nil, errors.New("erro ao acessar banco de dados")
 	}
 
 	return &DBConnectionHandler{DB: handler}, nil
 }
 
+// Close encerra a conexão com o banco de dados de maneira adequada.
 func (handler *DBConnectionHandler) Close() {
+	// Obtém a instância do banco de dados subjacente para poder fechar a conexão
 	db, err := handler.DB.DB()
 	if err != nil {
-		fmt.Printf("erro ao fechar e acessar banco de dados: %v", err)
+		log.Printf("Erro ao acessar o banco de dados para fechar a conexão: %v", err)
+		return
 	}
 
+	// Tenta fechar a conexão com o banco de dados
 	if err := db.Close(); err != nil {
-		fmt.Printf("erro ao fechar conexão com banco de dados: %v", err)
+		log.Printf("Erro ao fechar conexão com banco de dados: %v", err)
+	} else {
+		log.Println("Conexão com o banco de dados fechada com sucesso.")
 	}
 }
