4bb9eac04085511f433245108aa8af1ea236878e
Turgho <victor.hugo3692111@gmail.com>
2025-02-24 20:23:16 -0300

    fix: error to found logs folder

--- backend/models/settings/logger.go
+++ backend/models/settings/logger.go
@@ -9,8 +9,17 @@ import (
 
 // Função para configurar o arquivo de log
 func SetupLogging() *os.File {
+	// Define o diretório de logs
+	logDir := "../logs"
+
+	// Garante que o diretório de logs existe
+	if err := os.MkdirAll(logDir, os.ModePerm); err != nil {
+		log.Fatalf("Erro ao criar diretório de logs: %s", err)
+	}
+
 	// Cria o nome do arquivo de log com base na data
-	logFileName := fmt.Sprintf("../logs/app-%s.log", time.Now().Format("2006-01-02"))
+	logFileName := fmt.Sprintf("%s/app-%s.log", logDir, time.Now().Format("2006-01-02"))
+
 	// Abre ou cria o arquivo de log
 	logFile, err := os.OpenFile(logFileName, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
 	if err != nil {
